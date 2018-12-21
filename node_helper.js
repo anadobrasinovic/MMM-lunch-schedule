1faa1a0b72b8ecd163217645365eb09de361dfd9
anadobrasinovic <ana.dobrasinovic@symphony.is>
2018-12-21 12:36:55 +0100

    init commit

---
+++ node_helper.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const NodeHelper = require("node_helper");
+const request = require("request");
+
+module.exports = NodeHelper.create({
+  start: function () {
+	let self = this;
+	console.log("Starting node helper for: " + self.name);
+	self.cache = {};
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+	let self = this;
+	if (notification === "FETCH_LUNCH_SCHEDULE") {
+	  self.fetchWeeklySchedule(payload);
+	}
+  },
+  fetchWeeklySchedule: function (payload) {
+	const self = this;
+	return self.fetch("GET", payload.SCHEDULE_URL);
+  },
+  fetch: function (method, url, body) {
+	const self = this;
+	request({
+		url: url,
+		method: method,
+		headers: {"cache-control": "no-cache"},
+		body: body,
+	  },
+	  function (error, response, body) {
+		if (error) {
+		  self.sendSocketNotification("ERROR_LUNCH_SCHEDULE", {error: error});
+		  return console.error("MMM-lunch-schedule: " + error);
+		}
+		self.sendSocketNotification("LUNCH_SCHEDULE", JSON.parse(body));
+	  });
+  },
+
+
+});
